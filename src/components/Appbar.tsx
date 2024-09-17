9b9af2984df331ef5ce233562b62a96d5cddb197
Kanchandeep Kaur <kanchandeep.kaur@lpinfotech.com>
2024-09-17 16:21:57 +0530

    worked on homepage

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -7,41 +7,27 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import { Login } from "@mui/icons-material";
 
 const pages = ["Home", "Company", "Services", "Contact"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
   );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <AppBar position="static" color="transparent">
+    <AppBar position="fixed" color="transparent" elevation={0}>
       <Container maxWidth="lg">
         <Toolbar disableGutters>
           <AdbIcon
@@ -56,7 +42,7 @@ function ResponsiveAppBar() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="#"
             sx={{
               mr: 2,
               fontSize: "25px",
@@ -140,35 +126,6 @@ function ResponsiveAppBar() {
             <Button variant="contained" color="primary" startIcon={<Login />}>
               Login
             </Button>
-            {/* <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: "center" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
           </Box>
         </Toolbar>
       </Container>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,10 +4,50 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid2";
 import Container from "@mui/material/Container";
-import { Button, Typography } from "@mui/material";
-import { ChevronRight } from "@mui/icons-material";
+import {
+  Button,
+  Divider,
+  LinearProgress,
+  Typography,
+} from "@mui/material";
+import {
+  ChevronRight,
+  IntegrationInstructions,
+  SettingsApplications,
+  StorageOutlined,
+  TableChart,
+  WorkspacePremium,
+} from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const HomePage = () => {
+  const [progressValues, setProgressValues] = useState([
+    { label: "DATA ANALYSIS", value: 0 },
+    { label: "SOCIAL ASSISTANT", value: 0 },
+    { label: "AI SOLUTIONS", value: 0 },
+  ]);
+
+  useEffect(() => {
+    const updatedValues = [80, 55, 20]; // The target values for each progress bar
+
+    const timers = progressValues.map(
+      (progress, index) =>
+        setTimeout(() => {
+          setProgressValues((prev) =>
+            prev.map((p, i) =>
+              i === index ? { ...p, value: updatedValues[i] } : p
+            )
+          );
+        }, (index + 1) * 1000) // Delay increases for each progress bar
+    );
+
+    // Cleanup timeouts when the component unmounts
+    return () => {
+      timers.forEach((timer) => clearTimeout(timer));
+    };
+  }, []);
+
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: "#fff",
     ...theme.typography.body2,
@@ -22,6 +62,7 @@ const HomePage = () => {
   return (
     <>
       <ResponsiveAppBar></ResponsiveAppBar>
+      {/* hero section */}
       <section className="hero-section">
         <Container maxWidth="lg">
           <Box sx={{ flexGrow: 1 }}>
@@ -29,8 +70,11 @@ const HomePage = () => {
               <Grid
                 size={{ xs: 12, sm: 6 }}
                 alignSelf="center"
-                height={"70vh"}
-                sx={{ justifyItems: "start", alignContent: "center" }}
+                height={{ xs: "55vh", md: "90vh" }}
+                sx={{
+                  justifyItems: "start",
+                  alignContent: "center",
+                }}
               >
                 <Typography
                   variant="h2"
@@ -69,7 +113,7 @@ const HomePage = () => {
                 </Button>
               </Grid>
               <Grid
-                size={{ xs: 6, sm: 6 }}
+                size={{ xs: 12, sm: 6 }}
                 sx={{ justifyItems: "start", alignContent: "center" }}
               >
                 <img
@@ -81,6 +125,243 @@ const HomePage = () => {
             </Grid>
           </Box>
         </Container>
+      </section>
+      {/* why choose us */}
+      <section>
+        <Container maxWidth="lg">
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={6}>
+              <Grid
+                size={{ xs: 12, sm: 6 }}
+                alignSelf="center"
+                sx={{ justifyItems: "start", alignContent: "center" }}
+              >
+                <Typography
+                  variant="h6"
+                  color="primary"
+                  sx={{
+                    fontFamily: "sans-serif",
+                    fontWeight: 500,
+                    mb: 1,
+                    display: { xs: "none", md: "block" },
+                  }}
+                >
+                  Why Chose Us?
+                </Typography>
+                <Typography
+                  variant="h4"
+                  sx={{
+                    fontFamily: "sans-serif",
+                    fontWeight: 800,
+                    mb: 3,
+                  }}
+                >
+                  We are master in data science & big data analysis
+                </Typography>
+                <p>
+                  Humoured likewise moderate. Much nor game son say feel. Fat
+                  make met can must form into gate.
+                </p>
+              </Grid>
+              {/* progress */}
+              <Grid
+                size={{ xs: 12, sm: 6 }}
+                sx={{ justifyItems: "start", alignContent: "center" }}
+              >
+                <Box sx={{ width: "100%" }}>
+                  {progressValues.map((progress, index) => (
+                    <Box key={index} mb={2}>
+                      <Item
+                        sx={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                          fontWeight: "700",
+                          color: "black",
+                        }}
+                        elevation={0}
+                      >
+                        <Typography>{progress.label}</Typography>
+                        <Typography>{Math.round(progress.value)}%</Typography>
+                      </Item>
+                      <LinearProgress
+                        variant="determinate"
+                        className="rounded"
+                        value={progress.value}
+                      />
+                    </Box>
+                  ))}
+                </Box>
+              </Grid>
+              {/* progress */}
+              <Grid
+                size={{ xs: 12, sm: 5 }}
+                alignSelf="center"
+                sx={{ justifyItems: "start", alignContent: "center" }}
+              >
+                <img
+                  src="https://validthemes.net/site-template/anada/assets/img/illustration/14.png"
+                  alt="hero-image"
+                  width={"100%"}
+                />
+              </Grid>
+              <Grid
+                size={{ xs: 12, sm: 7 }}
+                width={"100%"}
+                sx={{
+                  justifyItems: "start",
+                  alignContent: "center",
+                  padding: 0,
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    gap: "20px",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Item style={{ display: "flex", gap: "20px" }} elevation={0}>
+                    <Item
+                      sx={{
+                        padding: "15px",
+                        display: "flex",
+                        alignItems: "center",
+                        width: "100%",
+                        backgroundColor: "aliceblue",
+                      }}
+                    >
+                      <DeleteIcon color="primary" sx={{ fontSize: "30px" }} />
+                      <Divider
+                        orientation="vertical"
+                        flexItem
+                        sx={{ marginRight: "15px", marginLeft: "10px" }}
+                      />
+                      <Typography component="small" fontWeight={700}>
+                        Data Cleaning
+                      </Typography>
+                    </Item>
+                    <Item
+                      sx={{
+                        padding: "15px",
+                        display: "flex",
+                        alignItems: "center",
+                        width: "100%",
+                        backgroundColor: "aliceblue",
+                      }}
+                    >
+                      <IntegrationInstructions
+                        color="primary"
+                        sx={{ fontSize: "30px" }}
+                      />
+                      <Divider
+                        orientation="vertical"
+                        flexItem
+                        sx={{ marginRight: "15px", marginLeft: "10px" }}
+                      />
+                      <Typography component="small" fontWeight={700}>
+                        API Integration Service
+                      </Typography>
+                    </Item>
+                  </Item>
+                  <Item style={{ display: "flex", gap: "20px" }} elevation={0}>
+                    <Item
+                      sx={{
+                        padding: "15px",
+                        display: "flex",
+                        alignItems: "center",
+                        width: "100%",
+                        backgroundColor: "aliceblue",
+                      }}
+                    >
+                      <TableChart color="primary" sx={{ fontSize: "30px" }} />
+                      <Divider
+                        orientation="vertical"
+                        flexItem
+                        sx={{ marginRight: "15px", marginLeft: "10px" }}
+                      />
+                      <Typography component="small" fontWeight={700}>
+                        Table Storage
+                      </Typography>
+                    </Item>
+                    <Item
+                      sx={{
+                        padding: "15px",
+                        display: "flex",
+                        alignItems: "center",
+                        width: "100%",
+                        backgroundColor: "aliceblue",
+                      }}
+                    >
+                      <StorageOutlined
+                        color="primary"
+                        sx={{ fontSize: "30px" }}
+                      />
+                      <Divider
+                        orientation="vertical"
+                        flexItem
+                        sx={{ marginRight: "15px", marginLeft: "10px" }}
+                      />
+                      <Typography component="small" fontWeight={700}>
+                        SQL Database
+                      </Typography>
+                    </Item>
+                  </Item>
+                  <Item style={{ display: "flex", gap: "20px" }} elevation={0}>
+                    <Item
+                      sx={{
+                        padding: "15px",
+                        display: "flex",
+                        alignItems: "center",
+                        width: "100%",
+                        backgroundColor: "aliceblue",
+                      }}
+                    >
+                      <SettingsApplications
+                        color="primary"
+                        sx={{ fontSize: "30px" }}
+                      />
+                      <Divider
+                        orientation="vertical"
+                        flexItem
+                        sx={{ marginRight: "15px", marginLeft: "10px" }}
+                      />
+                      <Typography component="small" fontWeight={700}>
+                        Applicatication Services
+                      </Typography>
+                    </Item>
+                    <Item
+                      sx={{
+                        padding: "15px",
+                        display: "flex",
+                        alignItems: "center",
+                        width: "100%",
+                        backgroundColor: "aliceblue",
+                      }}
+                    >
+                      <WorkspacePremium
+                        color="primary"
+                        sx={{ fontSize: "30px" }}
+                      />
+                      <Divider
+                        orientation="vertical"
+                        flexItem
+                        sx={{ marginRight: "15px", marginLeft: "10px" }}
+                      />
+                      <Typography component="small" fontWeight={700}>
+                        Data Quality Audit
+                      </Typography>
+                    </Item>
+                  </Item>
+                </div>
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </section>
+      {/* why choose us */}
+      {/* services */}
+      <section>
+        <Container maxWidth="lg"></Container>
       </section>
     </>
   );
