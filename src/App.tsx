2f0ca7e565d391efbe0fd2e89b10b06216af3c90
Kanchandeep Kaur <kanchandeep.kaur@lpinfotech.com>
2024-09-18 18:51:40 +0530

    code-optimisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+
 function App() {
   return (
     <Router>
